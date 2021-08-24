d9a9eb6a5b9a3034bd45b465575c9e347b7178fd
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-08-24 11:36:51 +0300

    Background styled on mobile resolution

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const Home = lazy(() => import('./views/Home'))
 
 const Wrapper = styled.div`
   position: relative;
+  overflow-x: hidden;
   
   > div > div {
     z-index: 2;
@@ -22,7 +23,6 @@ const Wrapper = styled.div`
 `
 
 const StyledBackground = styled.div`
-  height: 100%;
   min-height: 100%;
   width: 100vw;
   position: absolute;
@@ -33,9 +33,35 @@ const StyledBackground = styled.div`
   background-image: url(${background});
   background-position: center center;    
   background-repeat: no-repeat;
-  background-attachment: fixed;
+  background-attachment: inherit;
   background-size: cover;
   z-index: 1;
+  
+  :after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 184px;
+    background: linear-gradient(rgba(28, 28, 28, 0) 0%, rgb(28, 28, 28) 100%);
+    bottom: -1px;
+  }
+  
+  @media screen and (max-width: 824px) {
+    min-height: 110%;  
+  }
+  
+  @media screen and (max-width: 704px) {
+    min-height: 120%;  
+  }
+  
+  @media screen and (max-width: 704px) {
+    min-height: 130%;  
+  }
+  
+  @media screen and (max-width: 368px) {
+    background-attachment: fixed;
+    min-height: 160%; 
+  }
 `
 
 const App: React.FC = () => {
--- src/views/Home/components/HomeGreetings.tsx
+++ src/views/Home/components/HomeGreetings.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
-// import Lottie from 'react-lottie-player'
 import { useTranslation } from 'react-i18next'
 import { Flex } from '@gravis.finance/uikit'
 import {
@@ -13,6 +12,7 @@ import {
   PolkadotIcon,
   SolanaIcon,
 } from '../../../components/Svg'
+import Union from '../../../assets/Union.png'
 
 // import greetingsAnimationJSON from '../../../assets/data/greetings-animation.json'
 
@@ -20,6 +20,7 @@ const GreetingsWrapper = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+  overflow-x: hidden;
 
   @media screen and (max-width: 480px) {
     display: flex;
@@ -59,7 +60,6 @@ const GreetingsWrapper = styled.div`
 //   @media screen and (max-width: 1024px) {
 //     font-size: 64px;
 //     overflow: hidden;
-//
 //     line-height: 64px;
 //   }
 //   @media screen and (max-width: 480px) {
@@ -111,6 +111,40 @@ const GreetingsWrapper = styled.div`
 //   }
 // `
 
+const Logo = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 206px;
+  height: 66px;
+  margin: auto;
+  margin-top: -75px;
+  top: 0; 
+  left: 0; 
+  bottom: 0; 
+  right: 0;
+  
+  @media screen and (max-width: 968px) {
+    display: none;
+  }
+`
+
+const LogoText = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 60%;
+  color: white;
+  
+  p {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 31px;
+  }
+`
+
 const StyledAppContainer = styled.div`
   height: 100%;
   padding: 32px 32px 0 32px;
@@ -135,7 +169,7 @@ const Body = styled.div`
 const MainText = styled.div<{ mobile?: boolean }>`
   color: white;
   width: fit-content;
-  height: 95px;
+  height: 1em;
   text-align: center;
   font-weight: 700;
   font-size: 85px;
@@ -148,6 +182,7 @@ const MainText = styled.div<{ mobile?: boolean }>`
   
   @media screen and (max-width: 1140px) {
     font-size: 45px;
+    // height: 50px;
   }
 
   @media screen and (max-width: 670px) {
@@ -155,21 +190,39 @@ const MainText = styled.div<{ mobile?: boolean }>`
   }
   
   @media screen and (max-width: 525px) {
-    font-size: 30px;
+    font-size: 24px;
   }
   
-  @media screen and (max-width: 430px) {
-    font-size: 25px;
+  // @media screen and (max-width: 432px) {
+  //   font-size: 24px;
+  // }
+  
+  @media screen and (max-width: 368px) {
+    font-size: 19px;
   }
 `
 
 const AdditionalText = styled.div`
-  margin-top: 14px;
+  margin-top: 35px;
   color: white;
   text-align: center;
-  font-family: 'Inter var'
+  font-family: 'Inter var';
+  font-style: italic;
   font-weight: 500;
-  font-size: 28px;
+  font-size: 26px;
+  
+  @media screen and (max-width: 670px) {
+    margin-top: 20px;
+  }
+  
+  @media screen and (max-width: 525px) {
+    font-size: 24px;
+    margin-top: 25px;
+  }
+  
+  @media screen and (max-width: 400px) {
+    font-size: 19px;
+  }
 `
 
 const Button = styled.a<{ type: string }>`
@@ -204,12 +257,12 @@ const Text = styled.p<{ small?: boolean }>`
   margin-top: 77px;
   font-weight: 500;
   font-size: 23px;
-  color: rgba(255, 215, 0, 1);
+  color: rgba(255, 255, 255, 1);
   text-shadow: 1px 1px 10px;
 
   ${({ small }) =>
-    small
-      ? `
+  small
+    ? `
         font-weight: 400;
         font-size: 18px;
         margin-bottom: 18px;
@@ -217,7 +270,7 @@ const Text = styled.p<{ small?: boolean }>`
             margin-bottom: 4px;
         }
     `
-      : ''}
+    : ''}
 
   @media screen and (max-width: 825px) {
     margin-top: 47px;
@@ -302,15 +355,15 @@ const NetworkItem = styled.div<{ disabled?: boolean }>`
   min-height: 110px;
   // height: 148px;
   background: ${({ disabled }) =>
-    disabled
-      ? 'linear-gradient(90.28deg, #242424 0%, #1F1F1F 100%)'
-      : 'linear-gradient(90.28deg, #292929 0%, #242424 100%), linear-gradient(90.28deg, #242424 0%, #1F1F1F 100%)'};
+  disabled
+    ? 'linear-gradient(90.28deg, #242424 0%, #1F1F1F 100%)'
+    : 'linear-gradient(90.28deg, #292929 0%, #242424 100%), linear-gradient(90.28deg, #242424 0%, #1F1F1F 100%)'};
   border: ${({ disabled }) => (disabled ? 'none' : '1px solid #2E2E2E')};
   box-sizing: border-box;
   box-shadow: ${({ disabled }) =>
-    disabled
-      ? 'inset 0px -1px 0px rgba(129, 129, 129, 0.15), inset 0px 4px 25px rgba(0, 0, 0, 0.25)'
-      : '4px 4px 12px rgba(0, 0, 0, 0.4), -4px -4px 12px rgba(255, 255, 255, 0.05)'};
+  disabled
+    ? 'inset 0px -1px 0px rgba(129, 129, 129, 0.15), inset 0px 4px 25px rgba(0, 0, 0, 0.25)'
+    : '4px 4px 12px rgba(0, 0, 0, 0.4), -4px -4px 12px rgba(255, 255, 255, 0.05)'};
   border-radius: 16px;
   position: relative;
   display: flex;
@@ -339,30 +392,38 @@ const DescriptionTextHeadline = styled.div<{ mobile?: boolean }>`
   font-weight: 700;
   font-size: 30px;
   padding: 0;
-  ${({ mobile }) => (mobile ? 'display: none;' : 'display: block;')}
+  // ${({ mobile }) => (mobile ? 'display: none;' : 'display: block;')}
+  @media screen and (max-width: 766px) {
+    margin-top: 25px;
+  }
+  
   @media screen and (max-width: 670px) {
-    ${({ mobile }) => (mobile ? 'display: block;' : 'display: none;')}
-    font-size: 17px;
+    font-size: 19px;
+  }
+  
+  @media screen and (max-width: 368px) {
+    margin-top: 25px;
+    font-size: 19px;
   }
 `
 
 const DescriptionText = styled.div<{ mobile?: boolean }>`
   color: white;
   max-width: 90%;
-  margin-top: 15px;
+  margin-top: 25px;
   font-family: 'Inter var';
   text-align: center;
   font-weight: 300;
   font-size: 20px;
   padding: 0;
   line-height: 25px;
-  ${({ mobile }) => (mobile ? 'display: none;' : 'display: block;')}
+  // ${({ mobile }) => (mobile ? 'display: none;' : 'display: block;')}
   @media screen and (max-width: 1440px) {
     max-width: 100%;
   }
   @media screen and (max-width: 670px) {
-    ${({ mobile }) => (mobile ? 'display: block;' : 'display: none;')}
-    font-size: 14px;
+    // ${({ mobile }) => (mobile ? 'display: block;' : 'display: none;')}
+    font-size: 19px;
   }
 `
 
@@ -370,102 +431,110 @@ const HomeGreetings: React.FC = () => {
   const descriptionRef = useRef()
   const { t } = useTranslation()
 
-  useEffect(()=>{
+  useEffect(() => {
     // @ts-ignore
     descriptionRef.current.innerHTML = t('getReady')
   }, [t])
 
   return (
-    <GreetingsWrapper>
-      <StyledAppContainer>
-        <Body>
-          <MainText>
-            {t('mainMessageDesktop.gamified')}
-          </MainText>
-          <MainText mobile>
-            {t('mainMessageDesktop.gamified')}
-          </MainText>
-          <AdditionalText>
-            {t('mainMessageDesktop.smartNFTAssets')}
-          </AdditionalText>
-          <Flex alignItems="center" style={{ marginTop: '25px' }}>
-            <Button type="default" href={t('presentationLink')} target="_blank">
-              <StyledArrowDownIcon />
-              {t('learnMore')}
-            </Button>
-            <InputContainer>
-              <Button type="danger" href={process.env.REACT_APP_EXCHANGE_URL}>
-                {t('tradeNow')}
+    <>
+      <Logo>
+        <img src={Union} alt="Gravis-Logo" />
+        <LogoText>
+          <p>Gravis</p>
+          <p>Finance</p>
+        </LogoText>
+      </Logo>
+      <GreetingsWrapper>
+        <StyledAppContainer>
+          <Body>
+            <MainText>
+              {t('mainMessageDesktop.gamified')}
+            </MainText>
+            <MainText mobile>
+              {t('mainMessageDesktop.gamified')}
+            </MainText>
+            <AdditionalText>
+              {t('mainMessageDesktop.smartNFTAssets')}
+            </AdditionalText>
+            <Flex alignItems="center" style={{ marginTop: '25px' }}>
+              <Button type="default" href={t('presentationLink')} target="_blank">
+                <StyledArrowDownIcon />
+                {t('learnMore')}
               </Button>
-            </InputContainer>
-          </Flex>
-          <DescriptionTextHeadline>{t('nftFarmingSoon')}</DescriptionTextHeadline>
+              <InputContainer>
+                <Button type="danger" href={process.env.REACT_APP_EXCHANGE_URL}>
+                  {t('tradeNow')}
+                </Button>
+              </InputContainer>
+            </Flex>
+            <DescriptionTextHeadline>{t('nftFarmingSoon')}</DescriptionTextHeadline>
 
-          <DescriptionText ref={descriptionRef}/>
-        </Body>
-        <Footer>
-          <NetworksContainer>
-            <Text small>{t('poweredBy')}</Text>
-            <Networks mobile>
-              <NetworkItem>
-                <HuobiIcon />
-              </NetworkItem>
-              <NetworkItem>
-                <BscIcon />
-              </NetworkItem>
-              <NetworkItem>
-                <InfIcon />
-              </NetworkItem>
-            </Networks>
-            <Networks>
-              <NetworkItem>
-                <HuobiIcon />
-              </NetworkItem>
-              <NetworkItem>
-                <BscIcon />
-              </NetworkItem>
-              <NetworkItem>
-                <InfIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <Text style={{ top: '-40px', width: '180px', position: 'absolute' }} small>
-                  {t('comingSoon')}
-                </Text>
-                <SolanaIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <PolkadotIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <NearIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <AIcon />
-              </NetworkItem>
-            </Networks>
-          </NetworksContainer>
-          <NetworksContainer mobile>
-            <Text small style={{ marginTop: '24px' }}>
-              {t('comingSoon')}
-            </Text>
-            <Networks mobile>
-              <NetworkItem disabled>
-                <SolanaIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <PolkadotIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <NearIcon />
-              </NetworkItem>
-              <NetworkItem disabled>
-                <AIcon />
-              </NetworkItem>
-            </Networks>
-          </NetworksContainer>
-        </Footer>
-      </StyledAppContainer>
-      {/* <InfoWrapper>
+            <DescriptionText ref={descriptionRef} />
+          </Body>
+          <Footer>
+            <NetworksContainer>
+              <Text small>{t('poweredBy')}</Text>
+              <Networks mobile>
+                <NetworkItem>
+                  <HuobiIcon />
+                </NetworkItem>
+                <NetworkItem>
+                  <BscIcon />
+                </NetworkItem>
+                <NetworkItem>
+                  <InfIcon />
+                </NetworkItem>
+              </Networks>
+              <Networks>
+                <NetworkItem>
+                  <HuobiIcon />
+                </NetworkItem>
+                <NetworkItem>
+                  <BscIcon />
+                </NetworkItem>
+                <NetworkItem>
+                  <InfIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <Text style={{ top: '-40px', width: '180px', position: 'absolute' }} small>
+                    {t('comingSoon')}
+                  </Text>
+                  <SolanaIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <PolkadotIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <NearIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <AIcon />
+                </NetworkItem>
+              </Networks>
+            </NetworksContainer>
+            <NetworksContainer mobile>
+              <Text small style={{ marginTop: '24px' }}>
+                {t('comingSoon')}
+              </Text>
+              <Networks mobile>
+                <NetworkItem disabled>
+                  <SolanaIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <PolkadotIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <NearIcon />
+                </NetworkItem>
+                <NetworkItem disabled>
+                  <AIcon />
+                </NetworkItem>
+              </Networks>
+            </NetworksContainer>
+          </Footer>
+        </StyledAppContainer>
+        {/* <InfoWrapper>
         <StyledHeading className="animate__animated animate__fadeInUp animate__delay-2s animate__fast ">
           {t('hurray')}!
         </StyledHeading>
@@ -480,10 +549,11 @@ const HomeGreetings: React.FC = () => {
         </a>
       </InfoWrapper> */}
 
-      {/* <ImageFrapper>
+        {/* <ImageFrapper>
       {animationInited && <Lottie loop={false} animationData={greetingsAnimationJSON} play />}
       </ImageFrapper> */}
-    </GreetingsWrapper>
+      </GreetingsWrapper>
+    </>
   )
 }
 
