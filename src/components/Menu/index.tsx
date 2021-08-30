e20e97b61b958eee48a91bb1c411c539344550c1
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-08-30 13:11:24 +0300

    Asteroid pitch deck added, analytics removed to exchange

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -34,6 +34,10 @@ const Menu: FC<PropsType> = ({ loginBlockVisible = true, ...props }) => {
           href: `${process.env.REACT_APP_CAPTAINS_URL}?${urlSearchLanguageParam}=${t('language')}`
         },
         {
+          label: '(A)steroid pitch deck',
+          href: `https://gateway.pinata.cloud/ipfs/QmWPNbXLtqh1gkXEe5BR5BLadGcz7sYAXjooSzrouBi9an`
+        },
+        {
           label: t('mainMenu.docs'),
           href: 'https://docs.gravis.finance',
           external: true
@@ -56,26 +60,30 @@ const Menu: FC<PropsType> = ({ loginBlockVisible = true, ...props }) => {
           label: t('mainMenu.migrate'),
           href: `${process.env.REACT_APP_MIGRATION_URL}?${urlSearchLanguageParam}=${t('language')}`,
         },
-      ],
-    },
-    {
-      label: t('mainMenu.analytics.analytics'),
-      icon: 'InfoIcon',
-      items: [
         {
-          label: t('mainMenu.analytics.overview'),
+          label: t('mainMenu.analytics.analytics'),
           href: `${process.env.REACT_APP_INFO_URL}?${urlSearchLanguageParam}=${t('language')}`,
         },
-        {
-          label: t('mainMenu.analytics.tokens'),
-          href: `${process.env.REACT_APP_INFO_URL}/tokens?${urlSearchLanguageParam}=${t('language')}`,
-        },
-        {
-          label: t('mainMenu.analytics.pairs'),
-          href: `${process.env.REACT_APP_INFO_URL}/pairs?${urlSearchLanguageParam}=${t('language')}`,
-        },
       ],
     },
+    // {
+    //   label: t('mainMenu.analytics.analytics'),
+    //   icon: 'InfoIcon',
+    //   items: [
+    //     {
+    //       label: t('mainMenu.analytics.overview'),
+    //       href: `${process.env.REACT_APP_INFO_URL}?${urlSearchLanguageParam}=${t('language')}`,
+    //     },
+    //     {
+    //       label: t('mainMenu.analytics.tokens'),
+    //       href: `${process.env.REACT_APP_INFO_URL}/tokens?${urlSearchLanguageParam}=${t('language')}`,
+    //     },
+    //     {
+    //       label: t('mainMenu.analytics.pairs'),
+    //       href: `${process.env.REACT_APP_INFO_URL}/pairs?${urlSearchLanguageParam}=${t('language')}`,
+    //     },
+    //   ],
+    // },
     {
       label: t('mainMenu.nftmarket'),
       icon: 'NFTMarketIcon',
