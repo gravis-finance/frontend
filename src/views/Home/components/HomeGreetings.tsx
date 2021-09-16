a89ae74e757ad00538a396e7dedbf2887cb23cbc
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-09-16 16:05:16 +0300

    Warnings deleted

--- src/views/Home/components/HomeGreetings.tsx
+++ src/views/Home/components/HomeGreetings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import { Flex, Input } from '@gravis.finance/uikit'
