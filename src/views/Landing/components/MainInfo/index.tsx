19402aea56d1048cfdb060975f850f5810212d55
darth_nihilus <darthnihilusgravis@gmail.com>
2022-03-11 17:43:56 +0300

    Add public round info on main page

--- src/views/Landing/components/MainInfo/index.tsx
+++ src/views/Landing/components/MainInfo/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button, Flex, Text } from '@gravis.finance/uikit'
-import { GravisBoldLogo } from '../../../../components/Svg'
+import { GravisBoldLogo, PublicRoundIcon } from '../../../../components/Svg'
 
 const Container = styled(Flex)`
-  padding-top: 70px;
   height: 100%;
+  padding: 298px 0 54px 0;
 `
 
 const MainText = styled(Text)`
@@ -28,31 +28,57 @@ const Description = styled(Text)`
 
 const ButtonsContainer = styled(Flex)``
 
+const PublicRoundContainer = styled(Flex)`
+  width: 432px;
+  height: 65px;
+  left: 504px;
+  top: 782px;
+
+  background: rgba(255, 255, 255, 0.05);
+  backdrop-filter: blur(45px);
+
+  border-radius: 47px;
+  padding: 32px 16px 32px 24px;
+`
+
 const MainInfo = () => {
   return (
-    <Container justifyContent="center" alignItems="center" flexDirection="column">
-      <Flex alignItems="center">
-        <GravisBoldLogo />
-        <MainText ml="8px">Gravis Finance</MainText>
+    <Container justifyContent="space-between" alignItems="center" flexDirection="column">
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Flex alignItems="center">
+          <GravisBoldLogo />
+          <MainText ml="8px">Gravis Finance</MainText>
+        </Flex>
+        <Text fontSize="56px" bold mt="12px" style={{ letterSpacing: '-0.02em' }}>
+          Gamified DeFi Ecosystem
+        </Text>
+        <Description>
+          {'Gravis Finance is an ecosystem that unites DeFi platform, NFT marketplace\n for in-game assets, and Evervoid game. We provide all the necessary\n instruments for managing your crypto assets on any chain.'
+            .split('\n')
+            .map((str) => (
+              <p>{str}</p>
+            ))}
+        </Description>
+        <ButtonsContainer mt={44}>
+          <Button variant="blue" style={{ letterSpacing: '-0.02em' }}>
+            Show Apps
+          </Button>
+          <Button variant="darkened" ml={16} style={{ letterSpacing: '-0.02em' }}>
+            Read Pitch Deck
+          </Button>
+        </ButtonsContainer>
       </Flex>
-      <Text fontSize="56px" bold mt="12px" style={{ letterSpacing: '-0.02em' }}>
-        Gamified DeFi Ecosystem
-      </Text>
-      <Description>
-        {'Gravis Finance is an ecosystem that unites DeFi platform, NFT marketplace\n for in-game assets, and Evervoid game. We provide all the necessary\n instruments for managing your crypto assets on any chain.'
-          .split('\n')
-          .map((str) => (
-            <p>{str}</p>
-          ))}
-      </Description>
-      <ButtonsContainer mt={44}>
-        <Button variant="blue" style={{ letterSpacing: '-0.02em' }}>
-          Show Apps
+      <PublicRoundContainer alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <PublicRoundIcon />
+          <Text fontSize="14px" ml="8px" style={{ letterSpacing: '0' }}>
+            Public Round Whitelist Access
+          </Text>
+        </Flex>
+        <Button variant="light" p="0 16px" style={{ height: '35px', fontSize: '12px', letterSpacing: '-0.02em' }}>
+          Learn more
         </Button>
-        <Button variant="darkened" ml={16} style={{ letterSpacing: '-0.02em' }}>
-          Read Pitch Deck
-        </Button>
-      </ButtonsContainer>
+      </PublicRoundContainer>
     </Container>
   )
 }
