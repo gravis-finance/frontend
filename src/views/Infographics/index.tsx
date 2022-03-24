b154ab498ef5c281384c0e88e1bf2935f64de768
Darth Nihilus <zotovprog@gmail.com>
2022-03-24 09:08:42 +0000

    Feature/infographics

---
+++ src/views/Infographics/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Box, Flex } from '@gravis.finance/uikit'
+import styled from 'styled-components'
+import t02 from 'assets/infographics/t02.png'
+import ecosystem from 'assets/infographics/ecosystem.png'
+import Page from '../../components/layout/Page'
+import GRVSTokenUtility from './components/GRVSTokenUtility'
+import GRVXTokenUtility from './components/GRVXTokenUtility'
+import EvervoidGameAssetCycle from './components/EvervoidGameAssetCycle'
+import { PlayText } from '../../components/PlayText'
+// import * as styles from '../Landing/styles'
+
+const Image = styled.img`
+  width: fit-content;
+  max-width: 600px;
+`
+
+// const Container = styled(Box).attrs((props) => ({
+//   position: 'relative',
+//   pt: styles.headerHeight,
+//   height: { md: styles.vh100 } as any,
+//   ...props,
+// }))`
+//   width: 100%;
+//   color: white;
+//   background-color: #1c1c1c;
+// `
+
+const Infographics = () => {
+  return (
+    <Page>
+      <Box>
+        <PlayText fontSize="64px" mb="48px">
+          Gravis Finance Infographics
+        </PlayText>
+        <PlayText fontSize="48px" mb="24px">
+          Ecosystem
+        </PlayText>
+        <Flex justifyContent="center" mb="64px">
+          <Image src={ecosystem} style={{ maxWidth: '80%' }} />
+        </Flex>
+        <GRVSTokenUtility />
+        <Box mt="64px">
+          <GRVXTokenUtility />
+        </Box>
+        {/* <Container mt="64px">
+          <Roadmap />
+        </Container> */}
+        <PlayText fontSize="48px" mt="64px" mb="16px">
+          Evervoid
+        </PlayText>
+        <EvervoidGameAssetCycle />
+        <PlayText fontSize="36px" mb="16px" mt="32px">
+          T02 NFT Crafting
+        </PlayText>
+        <Flex justifyContent="center">
+          <Image src={t02} />
+        </Flex>
+      </Box>
+    </Page>
+  )
+}
+
+export default Infographics
